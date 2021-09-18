cef6bc526b4a6afad0ee987ded25756e5bf5f54d
jujin1324 <jujin1324@daum.net>
2021-09-18 17:21:25 +0900

    정렬 테스트

--- src/test/java/jpabook/querydsl/QueryDslBasicTest.java
+++ src/test/java/jpabook/querydsl/QueryDslBasicTest.java
@@ -103,4 +103,24 @@ public class QueryDslBasicTest {
                 .selectFrom(member)
                 .fetchCount();
     }
+
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = members.get(0);
+        Member member6 = members.get(1);
+        Member memberNull = members.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+    }
 }
